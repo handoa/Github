070c48f75c3a8b3861da5ae62e1bfaecd3148c2e
juyeon <wonjy0307@naver.com>
2024-01-21 02:00:25 +0900

    로그인 화면

---
+++ app/src/main/java/com/example/github/activity_login.kt
@@ -0,0 +1,26 @@
+package com.example.github
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var loginButton: Button
+    lateinit var joinButton: Button
+    lateinit var idEditText: EditText
+    lateinit var passwordEditText: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        loginButton = findViewById(R.id.loginButton)
+        joinButton = findViewById(R.id.joinButton)
+        idEditText = findViewById(R.id.idEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
+
+
+    }
+}
