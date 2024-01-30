11c665f2237a5781a48a448b1b4a73b1014b22d4
doyoung <dy5644@naver.com>
2024-01-30 23:24:59 +0900

    ootd 페이지 구현

--- app/src/main/java/com/example/github/activity_ootd.kt
+++ app/src/main/java/com/example/github/activity_ootd.kt
@@ -3,12 +3,21 @@ package com.example.github
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.ImageView
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.widget.Toolbar
 
 class activity_ootd : AppCompatActivity() {
     lateinit var toolbar: Toolbar
     lateinit var actionBar: ActionBar
+    lateinit var imageView1 : ImageView
+    lateinit var imageView2 : ImageView
+    lateinit var imageView3 : ImageView
+    lateinit var imageView4 : ImageView
+    lateinit var imageView5 : ImageView
+    lateinit var imageView6 : ImageView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ootd)
@@ -17,6 +26,13 @@ class activity_ootd : AppCompatActivity() {
         setSupportActionBar(toolbar) //액티비티의 앱바로 지정
         actionBar = supportActionBar!!
         actionBar.setDisplayHomeAsUpEnabled(true) //뒤로가기 버튼 만들기
+
+        imageView1 = findViewById(R.id.imageView1)
+        imageView2 = findViewById(R.id.imageView2)
+        imageView3 = findViewById(R.id.imageView3)
+        imageView4 = findViewById(R.id.imageView4)
+        imageView5 = findViewById(R.id.imageView5)
+        imageView6 = findViewById(R.id.imageView6)
 
     }
 
